a02428d872529e48f6e27443e7ac9b66a05eb97e
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-05 12:36:44 +0530

    Update Config.py

--- src/Config.py
+++ src/Config.py
@@ -4,7 +4,7 @@ class Config:
     aid = int(os.environ.get("API_ID", None))
     ahash = os.environ.get("API_HASH", None)
     bot_token = os.environ.get("BOT_TOKEN", None)
-    sudo = [239508098, 1313665327]
+    sudo = [1165699179]
     # try:
     #     sudo = set(int(x) for x in os.environ.get("SUDO", "").split(','))
     # except ValueError:
